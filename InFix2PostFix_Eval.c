ab33e4dc747e901a56faebc7b5ac425c19e53fa0
seifmaazouz <154283002+seifmaazouz@users.noreply.github.com>
2024-05-01 00:03:19 +0300

    Added infixTopostfix function

--- InFix2PostFix_Eval.c
+++ InFix2PostFix_Eval.c
@@ -1,21 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
-typedef struct node {
+typedef struct node node;
+struct node {
     float data;
-    struct node *next;
-} node;
+     node *next;
+};
 
 typedef struct {
     node *head;
 } Stack;
 
-// PART 1 (stack functions)
+// PART 1: Stack Functions
 Stack* initialize ();
 float pop (Stack *s);
 void push (Stack *s, float value);
 float peek (Stack *s);
 int isEmpty (Stack *s);
+
+// PART 2: Infix to Postfix
+char* infixTopostfix (char *infix);
+int priority(char c);
+int isNum(char *str);
 
 
 Stack* initialize () {
@@ -98,11 +106,86 @@ void display(Stack *s) {
     printf("\n");
 }
 
+int priority (char c) {
+    switch (c)
+    {
+    case '+':
+    case '-':
+        return 1;
+    case '*':
+    case '/':
+        return 2;
+    case '^':
+        return 3;
+    default:
+        return 0;
+    }
+}
+
+char* infixTopostfix (char *infix) {
+    Stack *s = initialize();
+    char *post = malloc(strlen(infix)+1);
+    int j = 0;
+    char *token = strtok(infix, " ");
+    while(token != NULL) 
+    {
+       if (isNum(token))
+       {
+            strcpy(post + j*sizeof(char), token);
+            j += strlen(token);
+            post[j++] = ' ';
+       }
+        else if (isEmpty(s))
+            push(s, (float)(token[0]));
+        else if (token[0] == ')')
+        {
+            while (!isEmpty(s) && (char)(peek(s)) != '(')
+            {
+                post[j++] = (char)(pop(s));
+                post[j++] = ' ';
+            }
+            pop(s);
+        }
+        else
+        {
+            while (!isEmpty(s) && priority((char)(peek(s))) >= priority(token[0]) && token[0] != '(')
+            {
+                post[j++] = (char)(pop(s));
+                post[j++] = ' ';
+            }
+            push(s, (float)(token[0]));
+        }
+        token = strtok(NULL, " ");
+    }
+    while(!isEmpty(s)) 
+    {
+        post[j++] = (char)(pop(s));
+        if(!isEmpty(s))
+            post[j++] = ' ';
+    }
+    post[j] = '\0';
+    deconStack(s);
+    return post;
+}
+
+int isNum(char *str) {
+    char *endptr;
+    float num = strtof(str, &endptr);
+    if (endptr == str)
+        return 0;
+    else 
+        return 1;
+}
+
+
+
 int main()
 {
+    char infix[] = "2 + ( -2.5 + 3.14 ) * ( -5.4 + 8.1 ) ^ ( -0.5 )";
 
-    Stack *s = constStack();
-    deconStack(s);
+    char *post = infixTopostfix(infix);
+
+    printf("%s", post);
 
     return 0;
 }
